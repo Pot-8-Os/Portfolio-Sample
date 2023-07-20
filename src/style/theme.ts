b0d50927985adcac5eed2a0dabe15777d98bbe8a
sho <sho.hmd@gmail.com>
2023-07-20 09:35:11 +0900

    Create theme.ts

---
+++ src/style/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+import { purple, red } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: red[500],
+    },
+  },
+});
